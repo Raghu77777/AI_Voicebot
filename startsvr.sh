0a50ae05368c8abd2e61213ac705d70bc5a8427a
Raghu77777 <66983802+Raghu77777@users.noreply.github.com>
2020-06-16 08:42:39 +0530

    Update startsvr.sh

--- startsvr.sh
+++ startsvr.sh
@@ -1,2 +1,2 @@
-nohup python3 -m rasa_core_sdk.endpoint --actions actions &
+nohup python3 -m rasa_core_sdk.endpoint --actions actions & d
 nohup python3 -m rasa_core.run -d models/dialogue -u models/nlu/default/chat --debug --endpoints endpoints.yml --enable_api --port 80 --cors '*' &
